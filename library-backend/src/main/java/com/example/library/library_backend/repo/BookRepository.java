3c216e9c180335efa1705d46c4193106849b0b5e
ramasai1982 <ramasami.pannirselvame@gmail.com>
2025-02-13 11:39:03 +0100

    Book repo interface created

---
+++ library-backend/src/main/java/com/example/library/library_backend/repo/BookRepository.java
@@ -0,0 +1,15 @@
+package com.example.library.library_backend.repo;
+
+
+import com.example.library.library_backend.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByTitleContainingIgnoreCase(String title);
+    List<Book> findByAuthorContainingIgnoreCase(String author);
+}
+
