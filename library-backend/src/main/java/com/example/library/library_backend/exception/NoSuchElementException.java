147684a6956056cd7b866965342871ebb4181ed3
ramasai1982 <ramasami.pannirselvame@gmail.com>
2025-02-14 00:54:02 +0100

    NoSuchElementException deleted since handled with java.util

--- library-backend/src/main/java/com/example/library/library_backend/exception/NoSuchElementException.java
+++
@@ -1,9 +0,0 @@
-package com.example.library.library_backend.exception;
-
-public class NoSuchElementException extends RuntimeException {
-
-    public NoSuchElementException(String message) {
-        super(message);
-    }
-
-}
